e1138897d72df627ddaf2ee1d5b083173ed59ab7
evoicefire <gusbmc@gmail.com>
2015-11-30 23:38:58 +1100

    error handling, kinda broke ned to sleep

--- Sockets/SocketServ/SocketServ.py
+++ Sockets/SocketServ/SocketServ.py
@@ -6,6 +6,7 @@ __author__ = 'Gus'
 HOST = ''
 PORT = 8889
 p = 0
+e = 0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('--Socket created--')
 
@@ -24,16 +25,25 @@ print('Connection get {}'.format(addr))
 while 1:
     if p > 0:
         conn, addr = s.accept()
+        if e == 0:
+            print('Connection get {}'.format(addr))
     try:
         string = conn.recv(1024).decode("UTF-8")
+        e += 1
         print(string)
     except:
         try:
-            while 1:
-                string = conn.recv(4096).decode()
-                print(string)
+            try:
+                while 1:
+                    string = conn.recv(4096).decode()
+                    print(string)
+            except:
+                print('And unknown error occurred, lost client?')
+                pass
         except ConnectionResetError:
             print('Connection Reset')
+            e = 0
+            p = 0
             string = ''
             pass
     conn.close()
